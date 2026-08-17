79e3aa2b51653bfa064a844eeac99879e1a22228
Luka Boulpaep <lukaboulpaep@outlook.com>
2026-08-17 21:30:10 +0200

    feat(display): configure internal panel at 120 Hz

--- hosts/thinkpad/monitors.nix
+++ hosts/thinkpad/monitors.nix
@@ -3,12 +3,13 @@
 
   internal = {
     name = "eDP-1";
-    mode = "preferred";
+    mode = "2880x1800@120";
     position = "auto";
     scale = "auto";
   };
 
   hyprland = [
+    "eDP-1, 2880x1800@120, auto, auto"
     ", preferred, auto, auto"
   ];
 }
